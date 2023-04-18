fe61903eb6f1873cd5f06555b4fb24c12363279a
Morffo <120753366+Morffo@users.noreply.github.com>
2023-04-18 22:10:09 +0300

    Create klava.py

---
+++ bot/klava.py
@@ -0,0 +1,26 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+ikb = InlineKeyboardMarkup(row_width=4)
+ib1 = InlineKeyboardButton(text='Математика🎲', callback_data='math')
+ib2 = InlineKeyboardButton(text='Русский язык📔', callback_data='rus')
+ib3 = InlineKeyboardButton(text='Химия🧪', callback_data='chemistry')
+ib4 = InlineKeyboardButton(text='Биология🌱', callback_data='bio')
+ib5 = InlineKeyboardButton(text='Физика⚙️', callback_data='phy')
+ib6 = InlineKeyboardButton(text='Информатика💻', callback_data='inf')
+ikb.add(ib1, ib2).add(ib3, ib4).add(ib5, ib6)
+
+ikbb = InlineKeyboardMarkup(row_width=4)
+ibb1 = InlineKeyboardButton(text='Следующий курс ->', callback_data='next')
+ibb2 = InlineKeyboardButton(text='<- Предыдущий курс', callback_data='previous')
+ibb3 = InlineKeyboardButton(text='Перейти к выбору комманды', callback_data='menu')
+ibb4 = InlineKeyboardButton(text='Оплатить курс', callback_data='payment')
+ikbb.add(ibb2, ibb1)
+ikbb.add(ibb3, ibb4)
+
+ped = InlineKeyboardMarkup(row_width=4)
+ped1 = InlineKeyboardButton(text="Сохраненные курсы", callback_data='mine')
+ped2 = InlineKeyboardButton(text='Созданные курсы', callback_data='made')
+ped3 = InlineKeyboardButton(text='Создать курс', callback_data='create')
+ped4 = InlineKeyboardButton(text='Назад в меню', callback_data='menu')
+ped.add(ped1, ped2)
+ped.add(ped3, ped4)
