b7c7740d103c44fec2c46128cb9a09d1bafb27da
Morffo <120753366+Morffo@users.noreply.github.com>
2023-04-18 22:08:00 +0300

    Delete Courses.py

--- bot/Courses.py
+++
@@ -1,6 +0,0 @@
-opisanie = ['Обучение подготовке учащихся к профильному ЕГЭ', 'Курс по подготовке:Общие формулы 7-11 класс',
-            'Обучение подготовке учащихся к Егэ базового уровня']
-mat_course = [
-    'https://fikiwiki.com/uploads/posts/2022-02/1644886743_1-fikiwiki-com-p-kartinki-formuli-po-matematike-1.jpg',
-    'https://moodle.ivgpu.com/pluginfile.php/49550/course/overviewfiles/unnamed.jpg',
-    'https://adsstatic.adsfactory.ru/tmp_media/tmp_5513/media_5513m1535013389.jpeg']
