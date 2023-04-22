cfaaded40b0d5bd9b7eba394bf8d180036ed5a77
bana2232 <bana2232@yandex.ru>
2023-04-22 14:53:02 +0300

    v0.0.6

---
+++ site/loginform.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, DateField, FieldList
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    patronymic = StringField('Отчество')
+    date = DateField('Дата рождения', validators=[DataRequired()])
+
+    education = StringField('Образование', validators=[DataRequired()])
+    edu_name = StringField('Название образовательного учреждения', validators=[DataRequired()])
+    work = StringField('Место работы', validators=[DataRequired()])
+
+    position = StringField('Должность', validators=[DataRequired()])
+    category = FieldList('Ваша категория', validators=[DataRequired()])
+    speciality = StringField('Специальность', validators=[DataRequired()])
+    type = FieldList('1111', validators=[DataRequired()])
+
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
--- site/main.py
+++ site/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect
 from database.sql_functions import start_session, close_session, check_user_when_logging_in
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "104479aad65a9c7479ef411a75c58d4fe2cb2d3bd5653e9171f2ec4e996d9fce"
 
 
 @app.route('/')
@@ -24,6 +25,15 @@ def sign_in():
             return "Добро пожаловать"
 
         return "Всё, давай, салам алейкум"
+
+
+@app.route("/log_in")
+def log_in():
+    if request.method == "GET":
+        return render_template("registration.html")
+
+    elif request.method == "POST":
+        pass
 
 
 if __name__ == '__main__':
