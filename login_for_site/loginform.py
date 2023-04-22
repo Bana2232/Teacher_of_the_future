5911aeb20bc54788ee68e60bddb59479ae19a95f
bana2232 <bana2232@yandex.ru>
2023-04-22 16:01:55 +0300

    v0.0.6.3

--- login_for_site/loginform.py
+++ login_for_site/loginform.py
@@ -4,19 +4,19 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    surname = StringField('Фамилия', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()], description="ФФ")
     name = StringField('Имя', validators=[DataRequired()])
     patronymic = StringField('Отчество')
-    date = DateField('Дата рождения', validators=[DataRequired()])
+    # date = DateField('Дата рождения', validators=[DataRequired()])
 
     education = StringField('Образование', validators=[DataRequired()])
     edu_name = StringField('Название образовательного учреждения', validators=[DataRequired()])
     work = StringField('Место работы', validators=[DataRequired()])
 
     position = StringField('Должность', validators=[DataRequired()])
-    category = FieldList('Ваша категория', validators=[DataRequired()])
+    # category = FieldList('Ваша категория', validators=[DataRequired()])
     speciality = StringField('Специальность', validators=[DataRequired()])
-    type = FieldList('1111', validators=[DataRequired()])
+    # type = FieldList('1111', validators=[DataRequired()])
 
     password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Зарегистрироваться')
--- site/main.py
+++ site/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect
 from database.sql_functions import start_session, close_session, check_user_when_logging_in
-from site
+from login_for_site.loginform import LoginForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "104479aad65a9c7479ef411a75c58d4fe2cb2d3bd5653e9171f2ec4e996d9fce"
@@ -30,16 +30,12 @@ def sign_in():
 
 @app.route("/log_in", methods=["GET", "POST"])
 def log_in():
-    if request.method == "GET":
-        form = LoginForm()
+    form = LoginForm()
 
-        if form.validate_on_submit():
-            return redirect('/success')
+    if form.validate_on_submit():
+        return "Success"
 
-        return render_template("registration.html", title="Регистрация", form=form)
-
-    elif request.method == "POST":
-        pass
+    return render_template("registration.html", title="Регистрация", form=form)
 
 
 if __name__ == '__main__':
