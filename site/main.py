d02baa5e19d7d2c821e78668363029721cfb5fd6
bana2232 <bana2232@yandex.ru>
2023-05-03 23:37:35 +0300

    v0.0.8

--- site/main.py
+++ site/main.py
@@ -9,7 +9,6 @@ from database.models.users_courses import User_courses_class
 
 from database.start_init import start_init
 
-from database.sql_functions import start_session, close_session, check_user_when_logging_in
 from database.db_session import global_init
 
 from loginform import LoginForm
@@ -42,7 +41,7 @@ def log_in():
                      form.edu_name.data, form.work.data, form.position.data, "...", form.speciality.data, "...",
                      form.password.data]
         print(user_data)
-        return "success"
+        return redirect("main_menu.html")
 
     return render_template("registration.html", form=form)
 
@@ -50,7 +49,8 @@ def log_in():
 if __name__ == '__main__':
     global_init("../database/main_database.db")
     # start_init()
-    db_sess = db_session.create_session()
-    for i in db_sess.query(User_type).filter(User.id == 1):
-        print(i)
-    # app.run(port=8080, host='127.0.0.1')
+    # db_sess = db_session.create_session()
+    # for i in db_sess.query(User_type).filter(User_type.id == 1):
+    #     print(i)
+
+    app.run(port=8080, host='127.0.0.1')
